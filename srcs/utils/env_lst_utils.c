c062152608e8e354bed66cb1c26338491fe8751c
Baptiste Perriolat <bperriol@z2r10p6.42lyon.fr>
2023-01-24 14:03:36 +0100

    list env after merge

--- srcs/utils/env_lst_utils.c
+++ srcs/utils/env_lst_utils.c
@@ -6,54 +6,58 @@
 /*   By: bperriol <bperriol@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 13:32:11 by bperriol          #+#    #+#             */
-/*   Updated: 2023/01/24 14:02:25 by bperriol         ###   ########lyon.fr   */
+/*   Updated: 2023/01/24 14:03:24 by bperriol         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// t_env	*new_env(char *var, char *value, int has_equal)
-// {
-// 	t_env	*env;
+t_env	*new_env(char *var, char *value, int has_equal)
+{
+	t_env	*env;
 
-// 	new = malloc(sizeof(t_env));
-// 	if (!new)
-// 		return (perror("New_env: "), NULL);
-// 	new->var = var;
-// 	new->value = value;
-// 	new->has_equal = has_equal;
-// 	new->next = NULL;
-// 	return (new);
-// }
+	new = malloc(sizeof(t_env));
+	if (!new)
+		return (perror("New_env: "), NULL);
+	new->var = var;
+	new->value = value;
+	new->has_equal = has_equal;
+	new->next = NULL;
+	return (new);
+}
 
-// void	env_clear_data(t_env **env)
-// {
-// 	t_env	*clear;
-// 	t_env	*tmp;
+void	env_clear_data(t_env **env)
+{
+	t_env	*clear;
+	t_env	*tmp;
 
-// 	if (!env)
-// 		return ;
-// 	clear = *env;
-// 	while (clear)
-// 	{
-// 		tmp = clear;
-// 		clear = clear->next;
-// 		free(tmp);
-// 	}
-// 	*env = NULL;
-// }
+	if (!env)
+		return ;
+	clear = *env;
+	while (clear)
+	{
+		tmp = clear;
+		clear = clear->next;
+		if (tmp->var)
+			free(tmp->var);
+		if (tmp->value)
+			free(tmp->value);
+		free(tmp);
+	}
+	*env = NULL;
+}
 
-// void	env_add_back(t_env **env, t_env *new)
-// {
-// 	t_env	*current;
+void	env_add_back(t_env **env, t_env *new)
+{
+	t_env	*current;
 
-// 	if (env && *env)
-// 	{
-// 		current = *env;
-// 		while (current->next)
-// 			current = current->next;
-// 		current->next = new;
-// 	}
-// 	else if (env)
-// 		*env = new;
-// }
+	if (env && *env)
+	{
+		current = *env;
+		while (current->next)
+			current = current->next;
+		current->next = new;
+	}
+	else if (env)
+		*env = new;
+}
