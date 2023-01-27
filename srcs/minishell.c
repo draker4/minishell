3439964d826e36d5b04c263609317acf9ea2494e
Baptiste Perriolat <bperriol@z2r12p2.42lyon.fr>
2023-01-27 11:45:27 +0100

    minishell ok

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: bperriol <bperriol@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 14:51:38 by bperriol          #+#    #+#             */
-/*   Updated: 2023/01/27 11:27:59 by bperriol         ###   ########lyon.fr   */
+/*   Updated: 2023/01/27 11:45:02 by bperriol         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,39 @@
 
 int	g_exit_status;
 
-void	print_exec(t_exec *exec)
-{
-	t_exec		*current;
-	t_redir		*redir;
-	int			i;
+// void	print_exec(t_exec *exec)
+// {
+// 	t_exec		*current;
+// 	t_redir		*redir;
+// 	int			i;
 
-	current = exec;
-	while (current)
-	{
-		printf("current = %s\n", current->str);
-		// i = 0;
-		// while (current->words[i])
-		// 	printf("detail current = %s et words = %s\n", current->str, current->words[i++]);
-		redir = current->redir;
-		while (redir)
-		{
-			printf("detail input = %s et type = %u\n", redir->str, redir->type);
-			redir = redir->next;
-		}
-		printf("\n");
-		if (current->arg)
-		{
-			i = 0;
-			while (current->arg[i])
-				printf("detail arg = %s\n", current->arg[i++]);
-		}
-		printf("\n");
-		printf("detail function = %s\n", current->function);
-		printf("is built in = %d\n", exec->cmd);
-		printf("\n");
-		current = current->next;
-	}
-}
+// 	current = exec;
+// 	while (current)
+// 	{
+// 		printf("current = %s\n", current->str);
+// 		// i = 0;
+// 		// while (current->words[i])
+// 		// 	printf("detail current = %s et words = %s\n", current->str, current->words[i++]);
+// 		redir = current->redir;
+// 		while (redir)
+// 		{
+// 			printf("detail input = %s et type = %u\n", redir->str, redir->type);
+// 			redir = redir->next;
+// 		}
+// 		printf("\n");
+// 		if (current->arg)
+// 		{
+// 			i = 0;
+// 			while (current->arg[i])
+// 				printf("detail arg = %s\n", current->arg[i++]);
+// 		}
+// 		printf("\n");
+// 		printf("detail function = %s\n", current->function);
+// 		printf("is built in = %d\n", exec->cmd);
+// 		printf("\n");
+// 		current = current->next;
+// 	}
+// }
 
 static int	read_line(t_data *data)
 {
@@ -65,7 +65,6 @@ static int	read_line(t_data *data)
 	{
 		if (parse(data->line, &data->exec_begin, data))
 			execute(data->exec_begin);
-			// print_exec(data->exec_begin);
 	}
 	else
 		g_exit_status = 0;
