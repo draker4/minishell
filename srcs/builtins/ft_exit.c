28a8f5d2c3d4d210a20b12eca7df62de5c4c48fb
Baptiste Perriolat <bperriol@z2r11p2.42lyon.fr>
2023-01-29 14:50:12 +0100

    all

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -6,11 +6,12 @@
 /*   By: bperriol <bperriol@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 02:30:17 by bperriol          #+#    #+#             */
-/*   Updated: 2023/01/29 13:17:53 by bperriol         ###   ########lyon.fr   */
+/*   Updated: 2023/01/29 14:19:59 by bperriol         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "math.h"
 
 extern int	g_exit_status;
 
@@ -32,11 +33,11 @@ static int	ft_is_num(char *str)
 	return (0);
 }
 
-static unsigned char	ft_long_atoi(char *str)
+static long long int	ft_long_atoi(char *str)
 {
 	size_t			i;
-	long double		result;
-	long double		sign;
+	long long int	result;
+	long long int	sign;
 
 	i = 0;
 	result = 0;
@@ -54,7 +55,7 @@ static unsigned char	ft_long_atoi(char *str)
 		result = result * 10 + str[i] - '0';
 		i++;
 	}
-	return ((unsigned char)(result * sign));
+	return (result * sign);
 }
 
 static int	ft_check_atoi(char *str)
@@ -82,7 +83,7 @@ static int	ft_check_atoi(char *str)
 
 void	ft_exit(t_exec *exec)
 {
-	unsigned char	code;
+	long long int	code;
 
 	write(1, "exit\n", 5);
 	if (size_arg(exec->arg) == 1)
@@ -93,7 +94,7 @@ void	ft_exit(t_exec *exec)
 	else if (size_arg(exec->arg) == 2)
 	{
 		code = ft_long_atoi(exec->arg[1]);
-		return (free_exit(exec, (int)code));
+		return (free_exit(exec, code));
 	}
 	write(2, "minishell: exit: too many arguments\n", 36);
 	g_exit_status = 1;
